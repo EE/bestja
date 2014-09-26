946b3525fbcf63f099c2f7de32c68337152759f3
Ludwik Trammer <ludwik@gmail.com>
2014-09-26 13:46:58 +0200

    Third tab in job offers view

--- addons/bestja_offers/models.py
+++ addons/bestja_offers/models.py
@@ -5,35 +5,75 @@ from lxml import etree
 from openerp import models, fields, api, exceptions
 
 
+class TargetGroup(models.Model):
+    """Odbiorca pomocy"""
+    _name = 'bestja.offers.target_group'
+    name = fields.Char()
+
+
 class Offer(models.Model):
     _inherit = 'hr.job'
     _states = [
-        ('open', 'not published'),
-        ('recruit', 'published')
+        ('open', 'nieopublikowana'),
+        ('recruit', 'opublikowana')
     ]
 
     state = fields.Selection(_states)
-    vacancies = fields.Integer(string='Number of vacancies')
-    project = fields.Many2one('project.project', required=True)
+    vacancies = fields.Integer(string="Liczba wakatów")
+    project = fields.Many2one('project.project', string="Projekt", required=True)
     skills = fields.Many2many('bestja.volunteer.skill')
     wishes = fields.Many2many('bestja.volunteer.wish')
-    drivers_license = fields.Many2one('bestja.volunteer.drivers_license')
-    sanepid = fields.Boolean(string="Sanepid certificate")
-    forklift = fields.Boolean(string="Forklift license")
+    drivers_license = fields.Many2one('bestja.volunteer.drivers_license', string="Prawa jazdy")
+    sanepid = fields.Boolean(string="Badania sanepidu")
+    forklift = fields.Boolean(string="Uprawnienia na wózek widłowy")
+    target_group = fields.Many2many(
+        'bestja.volunteer.occupation',
+        string="Kto jest adresatem oferty?",
+        help="Wybierz grupę docelową np. studenci, emeryci."
+    )
+    helpee_group = fields.Many2many(
+        'bestja.offers.target_group',
+        string="Komu wolontariusz może pomóc?",
+        help="Wybierz grupę osób, z którymi będzie pracował."
+    )
+    desc_aim = fields.Text(
+        string="Co jest celem oferty?",
+        help="""Opisz w skrócie czym będzie zajmował się wolontariusz. np. Akcja będzie polegała na uporządkowaniu trawnika"""
+    )
+    desc_expectations = fields.Text(
+        string="Co będzie robił wolontariusz?",
+        help="Jakie będą oczekiwania wobec wolontariusza. \
+        Co będzie musiał robić? np. Twoim zadaniem będzie grabienie liści, sadzenie trawy i krzewów"""
+    )
+    desc_why = fields.Text(
+        string="Jak praca wolontariusza przyczyni się do zmiany?",
+        help="Opisz dlaczego wolontariusz miałby się zaangażować w tą akcję, jaki problem pomoże rozwiązać, komu pomoże. \
+        np. Domy starców nie mają funduszy na rewitalizację zieleni, a starsze osoby często przebywają w ogrodzie. \
+        Twoja pomoc pozwoli seniorom miło spędzić czas w ogrodzie."
+    )
+    desc_benefits = fields.Text(
+        string="Jakie korzyści będzie miał wolontariusz?"
+    )
+    desc_tools = fields.Text(
+        string="Co zapewnia Twoja organizacja?"
+    )
+    desc_comments = fields.Text(
+        string="Uwagi"
+    )
 
     @api.one
     @api.constrains('skills')
     def _check_skills_no(self):
         max_skills = self.env.user.company_id.bestja_max_skills
         if len(self.skills) > max_skills:
-            raise exceptions.ValidationError("Choose no more than {} skills!".format(max_skills))
+            raise exceptions.ValidationError("Wybierz maksymalnie {} umiejętności!".format(max_skills))
 
     @api.one
     @api.constrains('wishes')
     def _check_wishes_no(self):
         max_wishes = self.env.user.company_id.bestja_max_wishes
         if len(self.wishes) > max_wishes:
-            raise exceptions.ValidationError("Choose no more than {} fields of interest!".format(max_wishes))
+            raise exceptions.ValidationError("Wybierz maksymalnie {} obszarów działania!".format(max_wishes))
 
     @api.model
     def fields_view_get(self, **kwargs):
