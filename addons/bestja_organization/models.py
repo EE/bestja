9d07c88ec4a8bd0ecbe7632215cdffd5bcbf1eb9
Julia Romanowska <romju57@gmail.com>
2014-10-10 17:07:33 +0200

    Changes after pull request.

--- addons/bestja_organization/models.py
+++ addons/bestja_organization/models.py
@@ -2,12 +2,9 @@
 
 from openerp import models, fields, api
 
-class BankiZywnosci(models.Model):
-    _name = 'organization.banki_zywnosci'
-    name = fields.Char()
-
 class Organization(models.Model):
     _name = 'organization'
+    _parent = 'parent'
 
     STATES_FOR_AUTHORIZATION = [
         ('waiting', 'Czeka na zatwierdzenie'),
@@ -15,24 +12,24 @@ class Organization(models.Model):
     ]
     state = fields.Selection(STATES_FOR_AUTHORIZATION, default='waiting', string="Stan")
     name = fields.Char(string="Nazwa", required=True)
-    krs = fields.Integer(size=10, string="KRS")
-    regon = fields.Integer(size=14, string="REGON")
-    nip = fields.Integer(size=10, string="NIP")
+    krs = fields.Char(string="KRS")
+    regon = fields.Char(string="REGON")
+    nip = fields.Char(string="NIP")
     street_address = fields.Char(string="Ulica", required=True)
     city_address = fields.Char(string="Miejscowość", required=True)
     street_number = fields.Char(string="Numer", required=True)
-    postal_code = fields.Integer(size=6,string="Kod pocztowy")
+    postal_code = fields.Char(size=6,string="Kod pocztowy")
     email = fields.Char(string="E-mail", required=True) 
-    phone = fields.Integer(size=10, string="Numer Telefonu")
+    phone = fields.Char(size=10, string="Numer Telefonu")
     www = fields.Char(string="WWW")
     fbfanpage = fields.Char(string="Fanpage na FB") 
-    bank_zywnosci = fields.Many2one('organization.banki_zywnosci', string="Bank Żywności")
-
-    coordinator = fields.Many2one('res.users', ondelete='set null', 
+    parent = fields.Many2one('organization')
+    
+    coordinator = fields.Many2one('res.users', ondelete='restrict', 
        string="Koordynator", default=lambda self: self.env.user, readonly=True) 
 
     active = fields.Boolean(default=True)
-
+   
     storage_street = fields.Char(string="Ulica")
     storage_street_number = fields.Char(string="Numer")
     storage_city_address = fields.Char(string="Miejscowość")
@@ -41,11 +38,8 @@ class Organization(models.Model):
     organization_description = fields.Text(string="Opis Organizacji")
 
     image = fields.Binary("Photo")
-    _sql_constraints = [
-        ('name_organization_uniq', 'CHECK(1=1)', ''),
-    ]
     
     @api.constrains('coordinator')
     def _check_organizations(self):
         if (self.coordinator.id != self.env.user.id): 
-            raise ValueError("Możesz zobaczyć tylko swoje organizacje") 
+            raise ValueError("Możesz zmieniać tylko swoje organizacje!") 
