fca2159154ec2b481b02d68c23651bc7ffa9b5cc
Ludwik Trammer <ludwik@gmail.com>
2014-11-06 15:16:30 +0100

    Organization profile

--- addons/bestja_organization/models.py
+++ addons/bestja_organization/models.py
@@ -12,13 +12,14 @@ class Organization(models.Model):
     name = fields.Char(string="Nazwa", required=True)
     krs = fields.Char(string="KRS")
     regon = fields.Char(string="REGON")
-    nip = fields.Char(string="NIP")
+    nip = fields.Char(required=True, string="NIP")
     street_address = fields.Char(string="Ulica", required=True)
     city_address = fields.Char(string="Miejscowość", required=True)
     street_number = fields.Char(string="Numer", required=True)
-    postal_code = fields.Char(size=6, string="Kod pocztowy")
+    postal_code = fields.Char(size=6, required=True, string="Kod pocztowy")
     email = fields.Char(string="E-mail", required=True)
-    phone = fields.Char(size=10, string="Numer Telefonu")
+    phone = fields.Char(required="True", string="Numer Telefonu")
+    phone_extension = fields.Char(size=10, string="Numer wewnętrzny")
     www = fields.Char(string="WWW")
     fbfanpage = fields.Char(string="Fanpage na FB")
     parent = fields.Many2one(
@@ -34,7 +35,11 @@ class Organization(models.Model):
         default=lambda self: self.env.user,
         groups="bestja_base.bestja_instance_admin"
     )
-    active = fields.Boolean(default=False, string="Zaakceptowana")
+    active = fields.Boolean(
+        default=False,
+        groups="bestja_base.bestja_instance_admin",
+        string="Zaakceptowana"
+    )
     organization_description = fields.Text(string="Opis Organizacji")
     image = fields.Binary("Photo")
 
@@ -67,10 +72,21 @@ class Organization(models.Model):
         control_sum = 0 if control_sum == 10 else control_sum
         return control_sum == digits[-1]
 
+    def clean_field_value(self, field_name):
+        """
+        Removes redundant characters from numeric fields
+        """
+        USELESS_CHARS = '-. /'
+        value = str(getattr(self, field_name))
+        if any(char in value for char in USELESS_CHARS):
+            value = value.translate(None, USELESS_CHARS)
+            setattr(self, field_name, value)
+
     @api.one
     @api.constrains('nip')
     def _check_nip(self):
         weights = (6, 5, 7, 2, 3, 4, 5, 6, 7)
+        self.clean_field_value('nip')
         if self.nip and not self.is_gov_id_valid(self.nip, weights):
             raise exceptions.ValidationError("Niepoprawny NIP.")
 
@@ -78,6 +94,7 @@ class Organization(models.Model):
     @api.constrains('regon')
     def _check_regon(self):
         weights = (8, 9, 2, 3, 4, 5, 6, 7)
+        self.clean_field_value('regon')
         if self.regon and len(self.regon) > 9:
             weights = (2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8)
 
@@ -87,5 +104,6 @@ class Organization(models.Model):
     @api.one
     @api.constrains('krs')
     def _check_krs(self):
+        self.clean_field_value('krs')
         if self.krs and (len(self.krs) != 10 or not self.krs.isdigit()):
             raise exceptions.ValidationError("Niepoprawny KRS.")
