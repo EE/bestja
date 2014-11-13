3763936c441e71d342add48ad5d9d0ee755c8560
Ludwik Trammer <ludwik@gmail.com>
2014-11-13 13:02:56 +0100

    Meta packages collecting dependencies for particular clients

---
+++ addons/bestja_configuration_fpbz/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bestja: FBŻ",
+    'summary': """Installation configuration for FPBŻ""",
+    'description': """Installation configuration for Federacja Polskich Banków Żywności""",
+    'author': "Laboratorium EE",
+    'website': "http://www.laboratorium.ee",
+    'version': '0.1',
+    'category': 'Specific Industry Applications',
+    'depends': [
+        'base',
+        'bestja_base',
+        'bestja_volunteer',
+        'bestja_organization',
+        'bestja_project',
+        'bestja_offers',
+        'quizzes',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ addons/bestja_configuration_ucw/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bestja: UCW",
+    'summary': """Installation configuration for UCW""",
+    'description': """Installation configuration for Uniwersyteckie Centrum Wolontariatu""",
+    'author': "Laboratorium EE",
+    'website': "http://www.laboratorium.ee",
+    'version': '0.1',
+    'category': 'Specific Industry Applications',
+    'depends': [
+        'base',
+        'bestja_base',
+        'bestja_volunteer',
+        'bestja_organization',
+        'bestja_project',
+        'bestja_offers',
+        'bestja_offers_moderation',
+    ],
+    'installable': True,
+    'application': True,
+}
