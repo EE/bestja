b5c5cac009dac69380a5347e0cfe1f06c2d14e5c
Ludwik Trammer <ludwik@gmail.com>
2014-12-22 16:25:18 +0100

    Add calendar view to tasks

--- addons/bestja_project/models.py
+++ addons/bestja_project/models.py
@@ -140,8 +140,8 @@ class Task(models.Model):
     user_assigned_task = fields.Boolean(
         compute='_user_assigned_task'
     )
-    date_start = fields.Datetime(required=True, string="od dnia")
-    date_stop = fields.Datetime(required=True, string="do dnia")
+    date_start = fields.Date(required=True, string="od dnia")
+    date_stop = fields.Date(required=True, string="do dnia")
     date_button_click_start = fields.Datetime(string="data rozpoczęcia")
     date_button_click_stop = fields.Datetime(string="data zakończenia")
     description = fields.Text(string="Opis zadania")
