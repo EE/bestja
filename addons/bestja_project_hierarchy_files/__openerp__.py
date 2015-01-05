844775c9ad5456fb2b4f46679eb4fdfca73e948c
Ludwik Trammer <ludwik@gmail.com>
2015-01-05 17:20:16 +0100

    Permissions for hierarchical per-project file repositories

---
+++ addons/bestja_project_hierarchy_files/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bestja: Hierarchical Project files",
+    'summary': "Permissions for hierarchical per-project file repositories",
+    'description': """
+Hierarchical Project files
+==========================
+Allows managers of child projects to view files
+associated with parent repositories.
+    """,
+    'author': 'Laboratorium EE',
+    'website': 'http://www.laboratorium.ee',
+    'version': '0.1',
+    'depends': [
+        'bestja_project_hierarchy',
+        'bestja_project_files',
+    ],
+    'data': [
+        'security/security.xml',
+    ],
+    'auto_install': True,
+}
