a44daf4b9e87043f7e56e2fce806e2bb20253b3f
Ludwik Trammer <ludwik@gmail.com>
2015-01-28 12:37:46 +0100

    Fix problems with replying to messages

--- addons/message_template/models.py
+++ addons/message_template/models.py
@@ -101,12 +101,11 @@ class MessageTemplateMixin(models.AbstractModel):
         """
         return self.env['mail.thread'].message_redirect_action()
 
-    @api.multi
     def message_post(self, *args, **kwargs):
         """
         Used by the messages module to reply in a thread.
         """
-        return self.env['mail.thread'].message_post(*args, **kwargs)
+        return self.pool['mail.thread'].message_post(*args, **kwargs)
 
     @api.model
     def _get_access_link(self, mail, partner):
