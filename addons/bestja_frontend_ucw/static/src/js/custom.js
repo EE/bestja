c30554b719fbe964856d30f8681b547545eabbfc
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-02-10 10:42:10 +0100

    Changes of paths of photos, classes and some corrections

--- addons/bestja_frontend_ucw/static/src/js/custom.js
+++ addons/bestja_frontend_ucw/static/src/js/custom.js
@@ -2,7 +2,11 @@ jQuery(window).ready(function () {
     /* Function which changes size of Main top photo snippet when window is resized */
     var windowResize = $(window).on('resize', function () {
         var height = $(this).height() - $("navbar-static-top").height() - 50;
-        $('.carousel').height(height);        
+        if(height > 1920){
+            $('.carousel').height(1080);
+        } else {
+            $('.carousel').height(height);
+        }
     });    
     /* End of function resize */    
 });
