50b7684c0f3c79bd653997f8d1bc618b2b2df9c8
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-02-13 12:27:22 +0100

    Changes of margins in simple pages and smooth scrolling Classes of container-fluid changed to container. Removed unnecessary class. Smooth scrolling function in custom.js 500ms, stop() added.

--- addons/bestja_frontend_ucw/static/src/js/custom.js
+++ addons/bestja_frontend_ucw/static/src/js/custom.js
@@ -8,5 +8,23 @@ jQuery(window).ready(function () {
             $('.carousel').height(height);
         }
     });    
-    /* End of function resize */    
+    /* End of function resize */
+    
+    /* Smoth scrolling */
+    
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            $('html,body').stop();
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                }
+            }
+        });
+    });
 });
