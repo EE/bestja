bedcac554a7e7ea9b374570cbfa938d921a408c8
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2015-02-16 19:30:31 +0100

    Hover on categories in home page
    New descriptions placements.
    Pictograms on hover - they are on our server now.
    Script changes images src's for green hover.

--- addons/bestja_frontend_ucw/static/src/js/custom.js
+++ addons/bestja_frontend_ucw/static/src/js/custom.js
@@ -2,16 +2,25 @@ jQuery(window).ready(function () {
     /* Function which changes size of Main top photo snippet when window is resized */
     var windowResize = $(window).on('resize', function () {
         var height = $(this).height() - $("navbar-static-top").height() - 50;
-        if(height > 1920){
+        if (height > 1920) {
             $('.carousel').height(1080);
         } else {
             $('.carousel').height(height);
         }
-    });    
+    });
     /* End of function resize */
-    
+
+    $(".category_link").hover(function () {
+        $('img', this).attr("src", function (index, attr) {
+            return attr.replace("off", "on");
+        });
+    }, function () {
+        $('img', this).attr("src", function (index, attr) {
+            return attr.replace("on", "off");
+        });
+    });
+
     /* Smoth scrolling */
-    
     $(function () {
         $('a[href*=#]:not([href=#])').click(function () {
             $('html,body').stop();
