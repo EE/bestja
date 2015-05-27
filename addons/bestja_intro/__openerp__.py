8a5be04c9981265fb14bdca12fe14f4d0c818f4d
Ludwik Trammer <ludwik@gmail.com>
2015-05-27 17:16:03 +0200

    New intro JS module

---
+++ addons/bestja_intro/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BestJa: User Intro",
+    'summary': "Teach user how to use the site.",
+    'description': """
+BestJa User Intro
+=======================
+Intro.js based intro teaching users about the site.""",
+    'author': "Laboratorium EE",
+    'website': "http://www.laboratorium.ee",
+    'version': '0.1',
+    'depends': [
+        'bestja_base',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/website_template.xml',
+    ],
+}
